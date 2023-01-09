62919658510007b4456d633fa5b3fd0f81cd997f
s-jje <117354616+s-jje@users.noreply.github.com>
2023-01-10 00:44:22 +0900

    [level 1] Title: K번째수, Time: 5.52 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for (int i = 0; i < commands.length; i++) {
+            answer[i] = Arrays.stream(array)
+                    .skip(commands[i][0] - 1)
+                    .limit(commands[i][1] - commands[i][0] + 1)
+                    .sorted()
+                    .toArray()[commands[i][2] - 1];
+        }
+        return answer;
+    }
+}
