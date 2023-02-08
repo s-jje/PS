6c81d4644c4a186be67374b0763e21cd3aad5087
s-jje <117354616+s-jje@users.noreply.github.com>
2023-02-09 00:42:59 +0900

    [level 2] Title: 피로도, Time: 0.04 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87946. 피로도/피로도.java
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// dungeons_rows는 2차원 배열 dungeons의 행 길이, dungeons_cols는 2차원 배열 dungeons의 열 길이입니다.
+int solution(int k, int** dungeons, size_t dungeons_rows, size_t dungeons_cols) {
+    int answer = -1;
+    return answer;
+}
