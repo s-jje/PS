bb5d285d2ff05b1a81e07cb76f571a8d95b62b03
s-jje <117354616+s-jje@users.noreply.github.com>
2023-03-20 04:22:29 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 222.94 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,13 +1,11 @@
 def solution(elements):
     sequence_set = set(elements)
+    length = len(elements)
 
-    for i in range(2, len(elements) + 1):
-        for j in range(len(elements)):
-            if j + i <= len(elements):
-                sequence_set.add(sum(elements[j:j + i]))
-            else:
-                first_length = len(elements) - j
-                second_end = i - first_length
-                sequence_set.add(sum(elements[j:]) + sum(elements[:second_end]))
+    for i in range(length):
+        partial_sum = 0
+        for j in range(i, i + length):
+            partial_sum += elements[j % length]
+            sequence_set.add(partial_sum)
 
     return len(sequence_set)
