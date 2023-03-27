270238ac6ae23fc4fed4570fa2fa95771a809b5a
s-jje <117354616+s-jje@users.noreply.github.com>
2023-03-28 04:02:54 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 2.53 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -2,7 +2,7 @@ from typing import List
 import re
 
 
-def solution(files: List):
+def solution(files: List[str]) -> List[str]:
     files.sort(key=lambda f: int(re.split('(\d+)', f, maxsplit=1)[1]))
     files.sort(key=lambda f: re.split('(\d+)', f, maxsplit=1)[0].lower())
     return files
